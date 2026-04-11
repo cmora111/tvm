6d954d359745a81339d62935d332b41a9ce80638
Carlos Mora <mora@spectrix.coom>
2026-04-11 14:48:05 -0400

    cleaned up to more inline with the readme file

--- tvm_config.py
+++ tvm_config.py
@@ -14,12 +14,6 @@ Categories = {
     "Select Window": {
         "Select window": [0, "None"]
     },
-    "Passwords": {
-        "github": [1, "echo \"JjM06212008\""]
-    },
-    "System Backup": {
-        "bkp_rsync.sh": [1, "/home/mora/bin/bkp_rsync.sh -s / -d /mnt/backups"]
-    },
     "Admin_CMDs": {
         "ps": [1, "ps axwwl"],
         "psa": [2, "ps axwwl | grep -i <name>"],
@@ -29,29 +23,6 @@ Categories = {
         "ns": [2, "ns"],
         "cd": [2, "cd <name>"],
         "find_grep": [2, r"find . -type f -exec grep -i <name> {} \; -print"],
-    },
-    "Cheatsheets": {
-        "List": [2, "ls -C /home/mora/.cheat"],
-        "Cheatsheet": [2, "cat /home/mora/.cheat/<name>"]
-    },
-    "LVM CMDs": {
-        "lvm help": [2, "cat /home/mora/.cheat/lvm | less"],
-        "pvcreate": [2, "sudo pvcreate <name>"],
-        "pvscan": [2, "sudo pvscan"],
-        "pvs": [2, "sudo pvs"],
-        "pvremove": [2, "sudo pvremove <name>"],
-        "vgcreate": [2, "sudo vgcreate <name>"],
-        "vgs": [2, "sudo vgs"],
-        "vgremove": [2, "sudo vgremove <name>"],
-        "lvcreate": [2, "sudo lvcreate <name>"],
-        "lvs": [2, "sudo lvs"],
-        "lvremove": [2, "sudo lvremove <name>"]
-    },
-    "Projects_CMDS": {
-        "New": [2, "pgc <name>; pgt <name>"],
-        "ChgDir": [2, "pgt <name>"],
-        "edit_README": [2, "pgt <name>; retext README.md"],
-        "Tests_Dir": [2, "pgt <name>; cd tests"]
     },
     "APT_CMDs": {
         "Update": [2, "sudo apt update"],
@@ -63,44 +34,9 @@ Categories = {
         "Fix-Broken": [2, "sudo apt --fix-broken install -y"]
     },
     "Applications": {
-        "dbeaver": [3, "/usr/bin/dbeaver > /dev/null 2>&1 &"],
         "Glances": [1, "glances"],
-        "Zoom": [3, "zoom"],
-        "Cura": [3, "/home/mora/bin/cura > /dev/null 2>&1 &"],
-        "prusa-slicer": [3, "/usr/bin/prusa-slicer > /dev/null 2>&1 &"],
-        "prusa-gcodeviewer": [3, "/usr/bin/prusa-gcodeviewer > /dev/null 2>&1 &"],
-        "Blender": [3, "/usr/bin/blender > /dev/null 2>&1 &"],
-        "Arduino": [3, "/home/mora/Projects/Arduino/arduino > /dev/null 2>&1 &"],
-        "Mu": [3, "/usr/bin/mu-editor > /dev/null 2>&1 &"],
-        "Visual": [3, "/usr/bin/code > /dev/null 2>&1 &"],
-        "Asciiquarium": [1, "/home/mora/bin/asciiquarium"],
-        "Openaudible": [3, "/home/mora/Projects/Openaudible/openaudible > /dev/null 2>&1 &"],
-        "xLights": [3, "/home/mora/Projects/xLights/xLights > /dev/null 2>&1 &"],
-        "trilium": [3, "trilium > /dev/null 2>&1 &"],
+        "Firefox": [1, "/usr/bin/firefox"],
         "Wireshark": [3, "sudo /usr/bin/wireshark &"]
-    },
-    "Print_3D_CMDs": {
-        "Mount sdh1": [2, "sudo mount /dev/sdh1 /mnt"],
-        "uMount sdh1": [2, "sudo umount /dev/sdh1"],
-        "cd_3D_Print": [2, "cd /home/mora/3D_Print/<name>"],
-        "mdir_3D_Print": [2, "mkdir -p /home/mora/3D_Print/<name> && cd /home/mora/3D_Print/<name> && mkdir -p gcode"],
-        "mkdir SD": [2, "mkdir -p /media/mora/79C0-64B7/<name>; cd /media/mora/79C0-64B7/<name>"],
-        "cp SD": [2, "cp * /media/mora/79C0-64B7/<name>"],
-        "mkdir_cp2SD": [2, "cd /home/mora/3D_Print/<name>; cp /home/mora/3D_Print/<name>/gcode/* /media/mora/79C0-64B7/<name>"]
-    },
-    "Git_CMDs": {
-        "Git Init": [2, "git init"],
-        "Git Add": [2, "git add <name>"],
-        "Git Commit": [2, "git commit"],
-        "Git Push": [2, "git push -u origin main"],
-        "Git Clone": [2, "git clone "]
-    },
-    "Ssh": {
-        "R400": [2, "ssh pi@r400"],
-        "Alienware": [2, "ssh -T mora@alienware"],
-    },
-    "DB": {
-        "Grocery": [2, "mysql -u root -p"]
     },
     "Vi": {
         "vim": [2, "vi <name>\r"],
